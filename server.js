df89a5e9a3bb36e645c79aafbabf12a40f8bea85
Aliya Shaikh <aliyashaikh2534@gmail.com>
2025-04-23 13:32:12 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+  origin: ["http://localhost:5500/"]
+}))
+
+app.get('/movies', async(req, res)=>{
+  const movies = await axios.get("https://freetestapi.com/api/v1/movies")
+  return res.send(movies.json());
+})
+
+app.listen(3000, ()=>{
+  console.log("server started 3000");
+})
